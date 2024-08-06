11525f6c91a808cde0f46df4a778f38bb915f205
mayphyocoder <m.mayphyozaw@gmail.com>
2024-08-06 16:30:58 +0700

    first commit

---
+++ src/com/dayone/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.dayone.java;
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Today is DayOne and Installation Lesson.");
+		System.out.println("How to install Java and Eclipse and How to use them.");
+		System.out.println("No 1. Create Project Folder Path and the Project Name. \n No 2. Create Package Name (com.--.java)");
+		System.out.println("No 3. Create Class Name for the project.");
+	}
+}
